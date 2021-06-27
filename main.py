411ddeff306853ba1676e7e4e47ae9e289cd1ecf
Yon Ploj <yon.ploj@gmail.com>
2021-06-27 20:04:31 +0200

    removed cached data

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ production = os.getenv("DEBUG") == '0';
 if not production:
 	print("Running in debug mode. Do not deploy in this state.");
 
-TIMETABLE = "failed to load";
+TIMETABLE = "not cached";
 try:
 	with open('timetable.json') as json_file:
 		TIMETABLE = json.load(json_file);
 except:
-	print("Failed to load timetable json.");
+	print("Failed to load timetable json from cache.");
 
 dayIndex = {
 	"MON": 0,
@@ -36,28 +36,7 @@ dayIndex = {
 	"FRI": 4
 	# weekends don't exist in urnik.fri
 }
-'''
-for (i in urnik) {
-  if (urnik[i].predmet.abbr == "ODV" && urnik[i].tip == "P") urnik[i].link = "https://teams.microsoft.com/l/channel/19%3abfaa863595024fcc91959ddf16e89030%40thread.tacv2/General?groupId=c76ad578-7a69-4b47-bd06-07521f0ab47d&tenantId=a6cc90df-f580-49dc-903f-87af5a75338e"
-  if (urnik[i].predmet.abbr == "ODV" && urnik[i].tip == "LV") urnik[i].link = "https://teams.microsoft.com/l/channel/19%3a946e2e041f794f19bfb77efd9d773acd%40thread.tacv2/Laboratorijske%2520vaje?groupId=c76ad578-7a69-4b47-bd06-07521f0ab47d&tenantId=a6cc90df-f580-49dc-903f-87af5a75338e"
-  if (urnik[i].predmet.abbr == "P1" && urnik[i].tip == "P") urnik[i].link = "https://uni-lj-si.zoom.us/j/7311330543?pwd=ZnBZZnpyU2pNSUF1dFIwOTlxblg2Zz09"
-  if (urnik[i].predmet.abbr == "P1" && urnik[i].tip == "LV") urnik[i].link = "https://teams.microsoft.com/l/channel/19%3a82a5038cac3b4e46a2059c40d2ee8843%40thread.tacv2/General?groupId=d308da2b-c295-4e5d-9ec3-141b7f5516d8&tenantId=a6cc90df-f580-49dc-903f-87af5a75338e"
-  if (urnik[i].predmet.abbr == "A1") urnik[i].link = "Ni linka :("
-  if (urnik[i].predmet.abbr == "DS1" && urnik[i].tip == "P") urnik[i].link = "https://uni-lj-si.zoom.us/j/93091769572?pwd=RlhwRzNWVDlWT3A0L2d5NXo0MmNFZz09"
-  if (urnik[i].predmet.abbr == "DS1" && urnik[i].tip == "V") urnik[i].link = "https://fmf-uni-lj-si.zoom.us/j/91779033051?pwd=dWdRV2JJanNOM0ZWUFBoTVlObDJ6Zz09"
-  if (urnik[i].predmet.abbr == "LINALG" && urnik[i].tip == "P") urnik[i].link = "https://fmf-uni-lj-si.zoom.us/j/91456042157?pwd=d1YrK3NWYzIzQ2h3WEpPb2hBZFRudz09"
-  if (urnik[i].predmet.abbr == "LINALG" && urnik[i].tip == "V") urnik[i].link = "https://uni-lj-si.zoom.us/j/99794927001?pwd=VGY0dHdlY0xIVWNKYndPemEzR2FWUT09"
-}
 
-2. semester
-for (i in urnik) {
-  if (urnik[i].predmet.abbr == "LINALG" && urnik[i].tip == "P") urnik[i].link = "https://fmf-uni-lj-si.zoom.us/j/91456042157?pwd=d1YrK3NWYzIzQ2h3WEpPb2hBZFRudz09"
-  if (urnik[i].predmet.abbr == "LINALG" && urnik[i].tip == "V") urnik[i].link = "https://uni-lj-si.zoom.us/j/99794927001?pwd=VGY0dHdlY0xIVWNKYndPemEzR2FWUT09"
-  if (urnik[i].predmet.abbr == "DS2" && urnik[i].tip == "V") urnik[i].link = "https://uni-lj-si.zoom.us/j/93273156085?pwd=M1phUVlMeEd3OTFPSlBhZkRGNDlJdz09"
-  if (urnik[i].predmet.abbr == "DS2" && urnik[i].tip == "P") urnik[i].link = "https://uni-lj-si.zoom.us/j/94616241161?pwd=S2FJT3dmbDVOMzNkS3NOa1RwbUNyQT09"
-  if (urnik[i].predmet.abbr == "P2" && urnik[i].tip == "LV") urnik[i].link = "https://teams.microsoft.com/l/channel/19%3a1c769b77d6994d83bfcd88a577852fe7%40thread.tacv2/General?groupId=fb0a47b2-d93d-4b80-aecd-ea617da5b323&tenantId=a6cc90df-f580-49dc-903f-87af5a75338e"
-}
-'''
 predmeti = {
 	"ODV_LV": {
 		"name": "Osnove digitalnih vezij",
